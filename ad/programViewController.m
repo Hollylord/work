a0d38bed1a11173f5ac741efb8d24f4196935239
Hollylord <inc@appletekiMacBook-Pro.local>
2015-07-22 10:23:00 +0800

    完善recommendview 并增加进度条

--- ad/programViewController.m
+++ ad/programViewController.m
@@ -8,9 +8,10 @@
 
 #import "programViewController.h"
 #import "INSSearchBar.h"
-
+#import "ASProgressPopUpView.h"
 
 @interface programViewController () <INSSearchBarDelegate>
+@property (weak, nonatomic) IBOutlet ASProgressPopUpView *progressView;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (nonatomic, strong) INSSearchBar *searchBarWithDelegate;
 @end
@@ -40,6 +41,13 @@
     UIView *recommendView = [[[NSBundle mainBundle] loadNibNamed:@"recommend" owner:self options:nil] firstObject];
     recommendView.frame = CGRectMake(6.5, segmentView.frame.origin.y + segmentView.frame.size.height, 362, 212);
     [self.scrollView addSubview:recommendView];
+    
+    //配置progressView 进度条
+    self.progressView.popUpViewCornerRadius = 12.0;
+    self.progressView.font = [UIFont fontWithName:@"Futura-CondensedExtraBold" size:28];
+    [self.progressView showPopUpViewAnimated:YES];
+    self.progressView.progress = 0.0;
+    [self progress];
     
     
     
@@ -80,4 +88,27 @@
     // Access the text from the search bar like searchBar.searchField.text
 }
 
+#pragma mark - 进度条
+- (void)progress
+{
+    float progress = self.progressView.progress;
+    if (self.progressView.progress < 1.0) {
+        
+        progress += 0.005;
+        
+        [self.progressView setProgress:progress animated:YES];
+        
+        
+        [NSTimer scheduledTimerWithTimeInterval:0.1
+                                         target:self
+                                       selector:@selector(progress)
+                                       userInfo:nil
+                                        repeats:NO];
+    }
+}
+
+- (BOOL)progressViewShouldPreCalculatePopUpViewSize:(ASProgressPopUpView *)progressView;
+{
+    return NO;
+}
 @end
