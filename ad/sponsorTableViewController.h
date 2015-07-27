ab5ad520b61dff3e3ed243f8710ef78ce879911c
Hollylord <inc@appletekiMacBook-Pro.local>
2015-07-27 18:12:35 +0800

    增加我关注的投资人

---
+++ ad/sponsorTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  sponsorTableViewController.h
+//  ad
+//
+//  Created by apple on 15/7/27.
+//  Copyright (c) 2015年 touwho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface sponsorTableViewController : UITableViewController
+
+@end
